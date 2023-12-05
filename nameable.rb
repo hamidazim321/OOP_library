d0c0156c830775f4dfebcc269a9a2d3db37177ee
hamid321 <hamidazim321@gmail.com>
2023-12-05 20:35:55 +0500

    Add decorators

--- nameable.rb
+++ nameable.rb
@@ -3,3 +3,30 @@ class Nameable
     raise NotImplementedError
   end
 end
+
+class Decorator < Nameable
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    super.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    return super[0, 9] if super.length > 10
+    super
+  end
+end
+
+
+
