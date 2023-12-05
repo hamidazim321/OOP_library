d2afdeadaff7c6b3c1f6cdbaf8580e5ee93780da
hamid321 <hamidazim321@gmail.com>
2023-12-05 21:40:15 +0500

    Add attr_accessor and change string length

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -1,7 +1,7 @@
 require_relative 'decorator'
 class TrimmerDecorator < Decorator
   def correct_name
-    return super[0, 10] if super.length > 10
+    return super[0, 9] if super.length > 10
 
     super
   end
