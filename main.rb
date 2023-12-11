bfc71c08d983dfad8228b8a821af8c1f7d18d59d
hamid321 <hamidazim321@gmail.com>
2023-12-11 18:55:06 +0500

    Remove extra code from main.rb file

--- main.rb
+++ main.rb
@@ -2,17 +2,6 @@ require_relative 'app'
 require_relative 'process_options'
 require_relative 'display_menu'
 
-def display_menu
-  puts 'Please choose an option by entering a number'
-  puts '1 - List all books'
-  puts '2 - List all people.'
-  puts '3 - Create a person'
-  puts '4 - Create a book'
-  puts '5 - Create a rental'
-  puts '6 - List all rentals for a given person id'
-  puts '7 - Exit'
-end
-
 def main
   app = App.new
 
