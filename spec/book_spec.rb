8b6f8d96907803c25d14567ad1b53f5de808c7a4
Fombi Favour <fombifavour@gmail.com>
2023-12-13 13:05:07 +0100

    Update book_spec.rb

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -2,7 +2,7 @@ require_relative '../book'
 
 describe 'testing the book class' do
   it 'should initialize with title and author' do
-    book = Book.new('The Great Gatsby', )
+    book = Book.new('The Great Gatsby', 'F. Scott Fitzgerald')
     expect(book.title).to eql 'The Great Gatsby'
     expect(book.author).to eql 'F. Scott Fitzgerald'
   end
