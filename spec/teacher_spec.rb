7ca61afa1dcdcb260e9aee643972dd5cda050d6e
hamid321 <hamidazim321@gmail.com>
2023-12-13 17:26:31 +0500

    Add tests for the Rental class

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../teacher'
-# def initialize(age, specialization, name = 'unknown', parent_permission: true)
 describe Teacher do
   context "When testing the Teacher class" do
     it "Should always return true on can_use_services? method" do
